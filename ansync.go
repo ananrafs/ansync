0d4fa189ab0b91f4ae5be2e71b11c153deec4ee2
ananrafs <ahmad.rafsanjani@tokopedia.com>
2024-04-16 10:49:13 +0700

    add do and doWithReturn

---
+++ ansync.go
@@ -0,0 +1,46 @@
+package main
+
+import "context"
+
+func Do(ctx context.Context, f func() error) error {
+	done := make(chan error, 1)
+	go func() {
+		err := f()
+		done <- err
+		close(done)
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case ret := <-done:
+		return ret
+	}
+}
+
+func DoWithReturn(ctx context.Context, f func() (interface{}, error)) (interface{}, error) {
+	done := make(chan interface{}, 1)
+	fail := make(chan error, 1)
+
+	go func() {
+		res, err := f()
+		if err != nil {
+			fail <- err
+			close(fail)
+			close(done)
+			return
+		}
+		done <- res
+		close(done)
+		close(fail)
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case ret := <-done:
+		return ret, nil
+	case ret := <-fail:
+		return nil, ret
+	}
+}
